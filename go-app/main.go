b3b06c650686ea6495923b8c0c68b3993d8469f4
Shubham Jha <shubhjha04578@gmail.com>
2024-06-21 20:35:19 +0530

    Update main.go

--- go-app/main.go
+++ go-app/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, World!")
+        fmt.Fprintf(w, "Hey There, This is just a sample GO App")
     })
 
     http.ListenAndServe(":5002", nil)
