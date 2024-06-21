0fc0ddd167166ca1c2f0b48fc220bc08f7741fdd
Shubham Jha <shubhjha04578@gmail.com>
2024-06-21 20:27:16 +0530

    Update index.js

--- nextjs-app/pages/index.js
+++ nextjs-app/pages/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 const InvalidPage = () => {
   return (
     <div>
-      <h3>This page is just a sample invalid react component</h3>
+      <h3>This page is just a sample invalid react component.</h3>
     </div>
   );
 };
